d02d1aeb65fd47b88d85a37a419ca0e30f28aefc
IhorVerba <ihor.verbivskyi@gmail.com>
2023-11-27 16:08:28 +0200

    make build

---
+++ index.bd007b73.js
@@ -0,0 +1,2 @@
+!function e(n,t){let r=document.createElement("ul");for(let l in n.append(r),t){let n=document.createElement("li");n.innerHTML=l,r.append(n),0!==Object.keys(t[l]).length&&e(n,t[l])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.bd007b73.js.map
